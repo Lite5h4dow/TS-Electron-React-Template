9848fcd4c05703e13da70b9ae056663133f0738d
Lamar Daughma <ldaughma@yozu.co.uk>
2021-01-09 20:17:14 +0000

    webpack additions

--- webpack.renderer.config.js
+++ webpack.renderer.config.js
@@ -1,14 +1,27 @@
 const rules = require('./webpack.rules');
 const plugins = require('./webpack.plugins');
 
-rules.push({
+// rules.push({
+//   test: /\.css$/,
+//   use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
+// });
+
+const test = [...rules, {
   test: /\.css$/,
-  use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
-});
+  loader: 'style-loader!css-loader'
+},
+{
+  test: /\.s[a|c]ss$/,
+  loader: 'sass-loader!style-loader!css-loader'
+},
+{
+  test: /\.(jpg|png|gif|jpeg|woff|woff2|eot|ttf|svg)$/,
+  loader: 'url-loader?limit=100000'
+}]
 
 module.exports = {
   module: {
-    rules,
+    rules: test,
   },
   plugins: plugins,
   resolve: {
